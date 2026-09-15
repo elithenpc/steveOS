8a9b6a1c0626de790a878ffb0481e370fc6497cf
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-15 20:11:37 +0930

    Add kernel foundation interface

---
+++ src/kernel.h
@@ -0,0 +1,10 @@
+#ifndef STEVEOS_KERNEL_H
+#define STEVEOS_KERNEL_H
+
+#include <efi.h>
+
+EFI_STATUS steveos_kernel_prepare(void);
+EFI_STATUS steveos_kernel_handoff(void);
+void steveos_kernel_panic(const CHAR8 *message);
+
+#endif
