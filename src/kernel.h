987be60083f77c0ea332ebf5c3a57faad929931d
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-15 21:43:08 +0930

    Expose native kernel boot entry

--- src/kernel.h
+++ src/kernel.h
@@ -4,6 +4,8 @@
 #include <efi.h>
 
 EFI_STATUS steveos_kernel_prepare(void);
+EFI_STATUS steveos_kernel_boot(EFI_HANDLE image_handle,
+                               EFI_GRAPHICS_OUTPUT_PROTOCOL *gop);
 EFI_STATUS steveos_kernel_handoff(void);
 void steveos_kernel_panic(const CHAR8 *message);
 
