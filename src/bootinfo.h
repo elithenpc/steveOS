05000f5e02e99270bee121271eddb3f0f1aaaf9a
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-15 21:41:10 +0930

    Add native kernel boot information structure

---
+++ src/bootinfo.h
@@ -0,0 +1,26 @@
+#ifndef STEVEOS_BOOTINFO_H
+#define STEVEOS_BOOTINFO_H
+
+#include <stdint.h>
+
+#define STEVEOS_BOOT_MAGIC 0x53544556454F5331ULL
+
+typedef struct {
+    uint64_t magic;
+    uint64_t framebuffer_base;
+    uint64_t framebuffer_size;
+    uint64_t width;
+    uint64_t height;
+    uint64_t pixels_per_scanline;
+    uint64_t pixel_format;
+    uint64_t memory_map;
+    uint64_t memory_map_size;
+    uint64_t memory_descriptor_size;
+    uint64_t memory_descriptor_version;
+    uint64_t acpi_rsdp;
+    uint64_t kernel_base;
+    uint64_t kernel_size;
+    uint64_t kernel_stack_top;
+} STEVEOS_BOOT_INFO;
+
+#endif
