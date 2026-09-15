40b83aaab359695153545f937d70c4a3f4a6eb6a
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-15 20:12:03 +0930

    Add task scheduler storage

---
+++ src/tasks.c
@@ -0,0 +1,26 @@
+#include "tasks.h"
+
+static STEVEOS_TASK tasks[STEVEOS_MAX_TASKS];
+static UINT32 task_count_value;
+
+EFI_STATUS steveos_tasks_init(void) {
+    SetMem(tasks, sizeof(tasks), 0);
+    task_count_value = 0;
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS steveos_task_create(UINT64 entry_point) {
+    if (task_count_value >= STEVEOS_MAX_TASKS || entry_point == 0)
+        return EFI_OUT_OF_RESOURCES;
+
+    STEVEOS_TASK *task = &tasks[task_count_value];
+    task->id = task_count_value + 1;
+    task->state = STEVEOS_TASK_READY;
+    task->instruction_pointer = entry_point;
+    task_count_value++;
+    return EFI_SUCCESS;
+}
+
+UINT32 steveos_task_count(void) {
+    return task_count_value;
+}
