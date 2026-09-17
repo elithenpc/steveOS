910f0f29387f6998b615d32161ba97d15c9029d2
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-18 08:32:23 +0930

    Fix browser image preview draw order

--- kernel/desktop.c
+++ kernel/desktop.c
@@ -843,8 +843,9 @@ static void draw_browser(void){
     else{
         fill_rect(34,184,(int)width-310,(int)height-310,bg_color());text_clip(52,196,browser_title[0]?browser_title:"UNTITLED",accent_color(),2,(int)width-350);
         int x=52,y=232,lines=0;const char*p=browser_text;int skip=browser_scroll;while(*p&&skip>0){if(*p=='\n')skip--;p++;}while(*p&&lines<((int)height-330)/16){int used=0;while(*p&&*p!='\n'&&used<((int)width-350)/6){glyph(x+used*6,y+lines*16,*p,text_color(),1);used++;p++;}while(*p&&*p!='\n')p++;if(*p=='\n')p++;lines++;}
+        fill_rect((int)width-286,184,252,(int)height-310,panel2_color());text((int)width-270,202,"LINKS",sub_color(),1);
         if(browser_image_loaded){draw_browser_image();if(browser_image_alt[0])text((int)width-270,372,browser_image_alt,text_color(),1);}
-        fill_rect((int)width-286,184,252,(int)height-310,panel2_color());text((int)width-270,202,"LINKS",sub_color(),1);for(int i=0;i<browser_link_count;i++){int yy=228+i*42;fill_rect((int)width-270,yy,220,32,panel_color());char num[2]={(char)('1'+i),0};text((int)width-258,yy+10,num,accent_color(),1);text_clip((int)width-238,yy+10,browser_link_text[i],text_color(),1,178);}
+        for(int i=0;i<browser_link_count;i++){int yy=228+i*42;fill_rect((int)width-270,yy,220,32,panel_color());char num[2]={(char)('1'+i),0};text((int)width-258,yy+10,num,accent_color(),1);text_clip((int)width-238,yy+10,browser_link_text[i],text_color(),1,178);}
     }
     text(40,(int)height-98,"HTTP BROWSER  •  CTRL+L ADDRESS  •  CTRL+D SAVE BOOKMARK  •  CTRL+B OPEN BOOKMARK",sub_color(),1);taskbar();
 }
