e7df566f38ef0fd80d559c10d87e4da3e0de894a
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-17 20:39:35 +0930

    Add native desktop header

---
+++ kernel/desktop.h
@@ -0,0 +1,6 @@
+#ifndef STEVEOS_DESKTOP_H
+#define STEVEOS_DESKTOP_H
+#include "../src/bootinfo.h"
+void steveos_desktop_init(STEVEOS_BOOT_INFO *boot);
+void steveos_desktop_run(STEVEOS_BOOT_INFO *boot);
+#endif
