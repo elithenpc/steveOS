69cb78df89659063bc85ad1b4c209c24de783dbf
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-17 17:09:26 +0930

    Add boot volume file handoff to native kernel

--- src/bootinfo.h
+++ src/bootinfo.h
@@ -4,6 +4,16 @@
 #include <stdint.h>
 
 #define STEVEOS_BOOT_MAGIC 0x53544556454F5331ULL
+#define STEVEOS_BOOT_FILE_NAME_MAX 64
+#define STEVEOS_MAX_BOOT_FILES 32
+
+typedef struct {
+    uint16_t name[STEVEOS_BOOT_FILE_NAME_MAX];
+    uint64_t size;
+    uint64_t data;
+    uint32_t attributes;
+    uint32_t kind;
+} STEVEOS_BOOT_FILE;
 
 typedef struct {
     uint64_t magic;
@@ -23,6 +33,9 @@ typedef struct {
     uint64_t kernel_stack_top;
     uint64_t uefi_get_variable;
     uint64_t uefi_set_variable;
+    uint64_t boot_files;
+    uint64_t boot_file_count;
+    uint64_t boot_device_handle;
 } STEVEOS_BOOT_INFO;
 
 #endif
